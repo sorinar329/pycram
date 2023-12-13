c47c5d386fe551ae327f0de94090968866ebeaf7
sl1ces <naser1@uni-bremen.de>
2023-12-13 11:37:18 +0100

    infer motion from swrl rules - mocked solution

--- demos/pycram_mixing_demo/mixing-demo.py
+++ demos/pycram_mixing_demo/mixing-demo.py
@@ -26,32 +26,32 @@ big_bowl_BO = BelieveObject(names=["big-bowl"])
 
 
 with simulated_robot:
-    # ParkArmsAction([Arms.BOTH]).resolve().perform()
-    # MoveTorsoAction([0.33]).resolve().perform()
-    # grasp = robot_description.grasps.get_orientation_for_grasp("top")
-    # arm = "left"
-    # #
-    # pickup_pose_knife = CostmapLocation(target=whisk_BO.resolve(), reachable_for=robot_desig).resolve()
-    # pickup_arm = pickup_pose_knife.reachable_arms[0]
+    ParkArmsAction([Arms.BOTH]).resolve().perform()
+    MoveTorsoAction([0.33]).resolve().perform()
+    grasp = robot_description.grasps.get_orientation_for_grasp("top")
+    arm = "left"
     #
+    pickup_pose_knife = CostmapLocation(target=whisk_BO.resolve(), reachable_for=robot_desig).resolve()
+    pickup_arm = pickup_pose_knife.reachable_arms[0]
+
+    NavigateAction(target_locations=[pickup_pose_knife.pose]).resolve().perform()
+
+    PickUpAction(object_designator_description=whisk_BO,
+                 arms=["left"],
+                 grasps=["top"]).resolve().perform()
+
+    ParkArmsAction([Arms.BOTH]).resolve().perform()
+    original_quaternion = (0, 0, 0, 1)
+    rotation_axis = (0, 0, 1)
+
+    rotation_quaternion = helper.axis_angle_to_quaternion(rotation_axis, 180)
+    resulting_quaternion = helper.multiply_quaternions(original_quaternion, rotation_quaternion)
+
+    nav_pose = Pose([-0.3, 0.9, 0.0], resulting_quaternion)
+
     # NavigateAction(target_locations=[pickup_pose_knife.pose]).resolve().perform()
-    #
-    # PickUpAction(object_designator_description=whisk_BO,
-    #              arms=["left"],
-    #              grasps=["top"]).resolve().perform()
-    #
-    # ParkArmsAction([Arms.BOTH]).resolve().perform()
-    # original_quaternion = (0, 0, 0, 1)
-    # rotation_axis = (0, 0, 1)
-    #
-    # rotation_quaternion = helper.axis_angle_to_quaternion(rotation_axis, 180)
-    # resulting_quaternion = helper.multiply_quaternions(original_quaternion, rotation_quaternion)
-    #
-    # nav_pose = Pose([-0.3, 0.9, 0.0], resulting_quaternion)
-    #
-    # # NavigateAction(target_locations=[pickup_pose_knife.pose]).resolve().perform()
-    # NavigateAction(target_locations=[nav_pose]).resolve().perform()
-    # LookAtAction(targets=[big_bowl_BO.resolve().pose]).resolve().perform()
+    NavigateAction(target_locations=[nav_pose]).resolve().perform()
+    LookAtAction(targets=[big_bowl_BO.resolve().pose]).resolve().perform()
     mixing_resolver = MixingActionSWRL(object_designator_description=big_bowl_BO,
                  object_tool_designator_description=whisk_BO,
                  arms=["left"],
--- src/pycram/resolver/action/mixing.py
+++ src/pycram/resolver/action/mixing.py
@@ -21,8 +21,8 @@ class MixingActionSWRL(MixingWhirlstormAction):
                  object_tool_designator_description: ObjectDesignatorDescription, arms: List[str],
                  grasps: List[str]):
         super().__init__(object_designator_description, object_tool_designator_description, arms, grasps)
-        self.knowledge_graph = get_ontology("/home/mkuempel/workspace/cram/src/PouringLiquids/src/mixing").load()
-        #self.knowledge_graph = MIXING
+        # self.knowledge_graph = get_ontology("/home/mkuempel/workspace/cram/src/PouringLiquids/src/mixing").load()
+        self.knowledge_graph = MIXING
 
     def name2owl(self):
         object_name = self.object_designator_description.names[0]
@@ -31,11 +31,29 @@ class MixingActionSWRL(MixingWhirlstormAction):
         owl_tool = MIXING.Whisk(tool_name)
         MIXING.Flour("flour")
         MIXING.Sugar("sugar")
+        MIXING.Water("water")
         MIXING.StirringTask("task")
         motion = MIXING.Motion("motion")
-        print(list(MIXING.rules()))
-        # sync_reasoner_pellet(infer_property_values=True, infer_data_property_values=True)
-        # print(motion.is_a)
+        superclasses_water = MIXING.Water.ancestors()
+        superclasses_flour = MIXING.Flour.ancestors()
+
+        union = superclasses_flour.union(superclasses_water)
+        intersection1 = set(MIXING.Ingredient.subclasses()).intersection(union)
+
+        rules = set()
+        for r in MIXING.rules():
+            body_classes = {pred.class_predicate for pred in r.body}
+            if len(body_classes.intersection(intersection1)) == len(intersection1):
+                rules.add(r)
+
+        other_rules = set(MIXING.rules()).difference(rules)
+        for r in other_rules:
+            destroy_entity(r)
+        for r in MIXING.rules():
+            print(r)
+
+        sync_reasoner_pellet(infer_property_values=True, infer_data_property_values=True)
+        print(motion.is_a)
 
     def parameters_from_owl(self):
         self.name2owl()
