5290f6dd06e1befbd3c091eefbedf507de22719b
naser1 <naser1@uni-bremen.de>
2024-01-10 14:50:29 +0100

    refactor mixing resolver

--- src/pycram/resolver/action/mixing.py
+++ src/pycram/resolver/action/mixing.py
@@ -1,76 +1,28 @@
 import dataclasses
+
+import owlready2
+import regex
 from ...designators.action_designator import (MixingWhirlstormAction)
 
 from ...designators.motion_designator import *
 
 from owlready2 import *
 
-from ontology.task import *
-from ontology.container import *
-from ontology.ingredient import *
-from ontology.motion import *
-from ontology.rules import onto
-
 SOMA = get_ontology("http://www.ease-crc.org/ont/SOMA.owl")
-MIXING = onto
 
 
 class MixingActionSWRL(MixingWhirlstormAction):
 
-    def __init__(self, object_designator_description: ObjectDesignatorDescription,
+    def __init__(self, object_container_designator_description: ObjectDesignatorDescription,
                  object_tool_designator_description: ObjectDesignatorDescription,
                  ingredients: List[str], task: str, arms: List[str], grasps: List[str]):
-        super().__init__(object_designator_description, object_tool_designator_description, arms, grasps)
-        # self.knowledge_graph = get_ontology("/home/mkuempel/workspace/cram/src/PouringLiquids/src/mixing").load()
+        super().__init__(object_container_designator_description, object_tool_designator_description, arms, grasps)
+
+        self.knowledge_graph = get_ontology("/home/naser/workspace/cram/src/PouringLiquids/src/mixing.owl").load()
         self.ingredients = ingredients
         self.task = task
-        self.knowledge_graph = MIXING
         self.motion = ""
         self.motion_parameters = {}
-
-    def name2owl(self):
-        container_instance, tool_instance = self.create_container_tool_instances()
-        ingredient_instances = self.create_ingredient_instances()
-        task_instance = self.create_task_instance()
-        motion = MIXING.Motion("motion")
-        union = set()
-
-        for ingredient_instance in ingredient_instances:
-            ing_ancestors = set(ingredient_instance.is_a[0].ancestors())
-            union = union.union(ing_ancestors)
-        intersection1 = set(MIXING.Ingredient.subclasses()).intersection(union)
-        rules = set()
-        for r in MIXING.rules():
-            body_classes = {pred.class_predicate for pred in r.body}
-
-            if len(body_classes.intersection(intersection1)) == len(intersection1)\
-                    and task_instance.is_a[0] in body_classes:
-                rules.add(r)
-
-        other_rules = set(MIXING.rules()).difference(rules)
-        for r in other_rules:
-            destroy_entity(r)
-
-        sync_reasoner_pellet(infer_property_values=True, infer_data_property_values=True)
-        motion_cls = motion.is_a[0]
-        self.motion = motion_cls.label[0].split('motion')[0].strip()
-
-        if MIXING.angle_shift1 in motion.get_properties():
-            self.motion_parameters.update({'angle_shift1': motion.angle_shift1[0]})
-        if MIXING.angle_shift2 in motion.get_properties():
-            self.motion_parameters.update({'angle_shift2': motion.angle_shift2[0]})
-        if MIXING.height_increment in motion.get_properties():
-            self.motion_parameters.update({'height_increment': motion.height_increment[0]})
-        if MIXING.radius_upper_bound in motion.get_properties() and MIXING.radius_lower_bound in motion.get_properties():
-            self.motion_parameters.update(
-                {'radius_bounds': [motion.radius_upper_bound[0], motion.radius_lower_bound[0]]})
-        if MIXING.vertical_increment in motion.get_properties():
-            self.motion_parameters.update({'vertical_increment': motion.vertical_increment[0]})
-
-        with MIXING:
-            for r in other_rules:
-                other_rule = Imp()
-                other_rule.set_as_rule(str(r), namespaces=[MIXING, SOMA])
 
     def create_ingredient_instances(self):
         ingredient_instances = []
@@ -106,8 +58,64 @@ class MixingActionSWRL(MixingWhirlstormAction):
                 task_instance = task(self.task)
         return task_instance
 
+    def assign_parameters(self, motion: owlready2.NamedIndividual):
+        motion_cls = motion.is_a[0]
+        self.motion = motion_cls.label[0].split('motion')[0].strip()
+
+        if self.knowledge_graph.angle_shift1 in motion.get_properties():
+            self.motion_parameters.update({'angle_shift1': motion.angle_shift1[0]})
+
+        if self.knowledge_graph.angle_shift2 in motion.get_properties():
+            self.motion_parameters.update({'angle_shift2': motion.angle_shift2[0]})
+
+        if self.knowledge_graph.height_increment in motion.get_properties():
+            self.motion_parameters.update({'height_increment': motion.height_increment[0]})
+
+        if self.knowledge_graph.radius_upper_bound_relative in motion.get_properties() and \
+                self.knowledge_graph.radius_lower_bound_relative in motion.get_properties():
+            self.motion_parameters.update(
+                {'radius_bounds': [motion.radius_upper_bound_absolute, motion.radius_lower_bound_absolute]})
+
+        if self.knowledge_graph.vertical_increment in motion.get_properties():
+            self.motion_parameters.update({'vertical_increment': motion.vertical_increment[0]})
+
+    def run_inference(self):
+        self.create_container_tool_instances()
+        ingredient_instances = self.create_ingredient_instances()
+        task_instance = self.create_task_instance()
+        union = set()
+
+        for ingredient_instance in ingredient_instances:
+            task_instance.ingredient.append(ingredient_instance)
+            ing_ancestors = set(ingredient_instance.is_a[0].ancestors())
+            union = union.union(ing_ancestors)
+        intersection1 = set(self.knowledge_graph.Ingredient.subclasses()).intersection(union)
+        rules = set()
+        for r in self.knowledge_graph.rules():
+            body_classes = {pred.class_predicate for pred in r.body}
+
+            if len(body_classes.intersection(intersection1)) == len(intersection1) \
+                    and task_instance.is_a[0] in body_classes:
+                rules.add(r)
+
+        other_rules = list(set(self.knowledge_graph.rules()).difference(rules))
+        for i in range(len(other_rules)):
+            name = other_rules[i].name
+            rule_as_string = str(other_rules[i])
+            destroy_entity(other_rules[i])
+            other_rules[i] = (name, rule_as_string)
+
+        sync_reasoner_pellet(infer_property_values=True, infer_data_property_values=True)
+
+        with self.knowledge_graph:
+            for name, rule_as_string in other_rules:
+                other_rule = Imp(name=name)
+                other_rule.set_as_rule(rule_as_string)
+
     def parameters_from_owl(self):
-        self.name2owl()
+        motion_instance = self.knowledge_graph.Motion("motion")
+        self.run_inference()
+        self.assign_parameters(motion_instance)
         # "Supported motions are: circular, folding, whirlstorm and vertical circular"
         # self.motion = "vertical circular"
         # self.motion_parameters = {"radius_bounds": [0.7, 0.0], "angle_shift1": 22.5, "angle_shift2": 90}
